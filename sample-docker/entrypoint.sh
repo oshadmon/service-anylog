6d719dce1c68879deb443b361e9cedb525c1260b
Ori Shadmon <oshadmon@gmail.com>
2023-10-29 13:04:07 -0700

    rewrite

--- sample-docker/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ ! ${LICENSE_KEY} == "" ]] ; then
-  python3.10  /app/start_anylog.py set license where activation_key=${LICENSE_KEY}
-else
-  python3.10  /app/start_anylog.py
-fi
